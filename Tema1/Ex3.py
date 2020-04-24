f0ce86ac48b8bea1d90dca3829336c9c4e15e66a
Tudor Mihai-Eugen <tudormihaieugen@gmail.com>
2020-04-24 14:33:18 +0300

    test

---
+++ Tema1/Ex3.py
@@ -0,0 +1,5 @@
+"""
+Creati un program in care utilizatorul sa introduca un an.
+Calculati daca anul este bisect sau nu si afisati un raspuns in acest sens.
+OBS: Un an bisect se imparte exact la 4 (fara rest).
+"""
