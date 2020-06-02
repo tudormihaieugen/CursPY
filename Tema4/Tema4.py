870826edab6c2ad4eb197090c28cf9e522996011
Tudor Mihai-Eugen <tudormihaieugen@gmail.com>
2020-06-02 18:49:05 +0300

    Tema4

--- Tema4/Tema4.py
+++ Tema4/Tema4.py
@@ -12,7 +12,7 @@ first = True
 for day in range(3, 25):
     string1 = 'https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-'
 
-    if day == 7 or day == 17:  #pentru 7 si 17 url-ul se formeaza altfel
+    if day == 7 or day == 17:  # pentru 7 si 17 url-ul se formeaza altfel
         string2 = '-aprilie-2020-ora-13-00-2/'
         req = requests.get(string1 + str(day - 1) + string2)
     else:
@@ -25,23 +25,23 @@ for day in range(3, 25):
 
     header.append(str(day) + " Aprilie")
     for i in title:
-        for tr_index in i.find_all('table', limit=1):  #in div se gasesc 2 tabele
+        for tr_index in i.find_all('table', limit=1):  # in div se gasesc 2 tabele
             index = 0
-            for td_index in tr_index.find_all('tr', limit=43)[1:]:  #primul element TR reprezenta capul de tabel
+            for td_index in tr_index.find_all('tr', limit=43)[1:]:  # primul element TR reprezenta capul de tabel
                 list_td = list()
 
                 for trd_index in td_index.find_all('td'):
                     list_td.append(trd_index.get_text().lstrip(' '))
 
-                if first:  #din primul link luam toate elementele din tabel
+                if first:  # din primul link luam toate elementele din tabel
                     data_list.append(list_td)
-                else:  #din urmatoarele luam doar coloana cu numar de cazuri
+                else:  # din urmatoarele luam doar coloana cu numar de cazuri
                     data_list[index].append(list_td[2])
 
                 index += 1
     first = False
 
-#construiesc din lista finala de date -> string pentru html
+# construiesc din lista finala de date -> string pentru html
 tr = ''
 for index in range(42):
     td = ''
