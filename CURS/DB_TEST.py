ff9fca309143971203c4833a7c5fc892c5b5074c
Tudor Mihai-Eugen <tudormihaieugen@gmail.com>
2020-07-15 21:48:25 +0300

    Final

--- CURS/DB_TEST.py
+++ CURS/DB_TEST.py
@@ -3,6 +3,6 @@ print("Connecting to database using MySQLdb")
 db_connection = MySQLdb.connect(host='localhost',
 								db='test',
 								user='root',
-								passwd='Depozitul123')
+								passwd='')
 print("Succesfully Connected to database using MySQLdb!")
 db_connection.close()
